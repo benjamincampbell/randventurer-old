ea389239c7a1ff0392671eb447a264936b7358d3
Benjamin Campbell <benjamin.m.campbell@wmich.edu>
2020-07-21 16:10:39 -0400

    Create Generator module, static files, test html page, add bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GeneratorComponent } from './generator/generator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: GeneratorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/races.ts
@@ -0,0 +1,64 @@
+export const races = [
+    {
+        name: 'Dragonborn',
+        stats: {
+            str: 2,
+            cha: 1,
+        }
+    },
+    {
+        name: 'Dwarf',
+        stats: {
+            con: 2,
+        }
+    },
+    {
+        name: 'Elf',
+        stats: {
+            dex: 2,
+        }
+    },
+    {
+        name: 'Gnome',
+        stats: {
+            int: 2,
+        }
+    },
+    {
+        name: 'Half-Elf',
+        stats: {
+            cha: 2,
+            other: 2,
+        }
+    },
+    {
+        name: 'Half-Orc',
+        stats: {
+            str: 2,
+            con: 1,
+        }
+    }
+    {
+        name: 'Halfling',
+        stats: {
+            dex: 2,
+        }
+    },
+    {
+        name: 'Human',
+        stats: {
+            str: 1,
+            dex: 1,
+            con: 1,
+            int: 1,
+            wis: 1,
+            cha: 1,
+        }
+    },
+    {
+        name: 'Tiefling',
+        stats: {
+            cha: 2,
+        }
+    },
+]
