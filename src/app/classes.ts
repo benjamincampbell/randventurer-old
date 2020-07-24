e361af777522a6e114b06868fdefd290c4db1f0d
Benjamin Campbell <benjamin.m.campbell@wmich.edu>
2020-07-23 22:17:08 -0400

    Added HP including con mod, assuming Lv 1

--- src/app/classes.ts
+++ src/app/classes.ts
@@ -1,38 +1,50 @@
 export const classes = [
     {
         name: 'Barbarian',
+        hitdie: 12,
     },
     {
         name: 'Bard',
+        hitdie: 8,
     },
     {
         name: 'Cleric',
+        hitdie: 8,
     },
     {
         name: 'Druid',
+        hitdie: 8,
     },
     {
         name: 'Fighter',
+        hitdie: 10,
     },
     {
         name: 'Monk',
+        hitdie: 8,
     },
     {
         name: 'Paladin',
+        hitdie: 10,
     },
     {
         name: 'Ranger',
+        hitdie: 10,
     },
     {
         name: 'Rogue',
+        hitdie: 8,
     },
     {
         name: 'Sorcerer',
+        hitdie: 6,
     },
     {
         name: 'Warlock',
+        hitdie: 8,
     },
     {
         name: 'Wizard',
+        hitdie: 6,
     },
 ];
